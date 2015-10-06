651adae91bb005a391e15b96e204c04df2648330
wesmangum <wesmangum24@gmail.com>
2015-10-06 12:27:58 -0500

    moce create method to controller

--- app/controllers/api/cars_controller.rb
+++ app/controllers/api/cars_controller.rb
@@ -21,4 +21,16 @@ class Api::CarsController < ApplicationController
 		render json: @car
 	end
 
+	def create
+		@car = Car.new
+
+		@car.make = params[:make]
+		@car.model = params[:model]
+		@car.year = params[:year]
+		@car.garage_id = params[:garage_id]
+
+		@car.save
+		redirect_to @car
+	end
+
 end
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,15 +1,3 @@
 class Car < ActiveRecord::Base
 	belongs_to :garage
-
-	def create
-		@car = Car.new
-
-		@car.make = params[:make]
-		@car.model = params[:model]
-		@car.year = params[:year]
-		@car.garage_id = params[:garage_id]
-
-		@car.save
-		redirect_to @car
-	end
 end
